d75ef5752912cf62053d0a6d228723beb0a131a0
MSW <mettasaridewi.w@gmail.com>
2020-03-01 01:43:39 +0700

    BLIND: masukin copas code scan keranjang

--- lib/scan_keranjang.dart
+++ lib/scan_keranjang.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 
 class ScanKeranjang extends StatelessWidget {
   static const routeName = '/scanKeranjang';
@@ -18,5 +18,80 @@ class ScanKeranjang extends StatelessWidget {
       ),
     );
     return scaffold;
+  }
+}
+*/
+
+import 'dart:async';
+
+import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class ScanKeranjang extends StatefulWidget {
+  static const routeName = '/scanKeranjang';
+  @override
+  _ScanState createState() => new _ScanState();
+}
+
+class _ScanState extends State<ScanKeranjang> {
+  String barcode = "";
+
+  @override
+  initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+          title: new Text('Scan Keranjang Belanja'),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    splashColor: Colors.blueGrey,
+                    onPressed: scan,
+                    child: const Text('START CAMERA SCAN')
+                ),
+              )
+              ,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                child: Text(barcode, textAlign: TextAlign.center,),
+              )
+              ,
+            ],
+          ),
+        ));
+  }
+
+  Future scan() async {
+    try {
+      String barcode = await BarcodeScanner.scan();
+      setState(() => this.barcode = barcode
+      //Navigator.pushNamed(context, '/listBarang');
+      );
+    } on PlatformException catch (e) {
+      if (e.code == BarcodeScanner.CameraAccessDenied) {
+        setState(() {
+          this.barcode = 'The user did not grant the camera permission!';
+        });
+      } else {
+        setState(() => this.barcode = 'Unknown error: $e');
+      }
+    } on FormatException{
+      setState(() => this.barcode = 'null (User returned using the "back"-button before scanning anything. Result)');
+    } catch (e) {
+      setState(() => this.barcode = 'Unknown error: $e');
+    }
   }
 }
